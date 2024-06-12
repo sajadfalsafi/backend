c082a5622c73a8e76cf98244358a00747e74c8f2
Zahra <zkarimi@bradford.ac.uk>
2024-06-12 18:37:05 +0100

    collect static files

--- sajad/settings.py
+++ sajad/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-bo+7u5pcl05_56el#9auofbtk*h=k9e$ulc$m0=m8-rm(pp#q-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sajadf.pythonanywhere.com']
 
 
 # Application definition
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
